496bbb54384b6f40ae7fc21ec312b7778ac0ee7a
Achref LAABIDI <abidi.achref95@gmail.com>
2023-11-22 17:29:38 +0100

    added nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+        <div className="navbar bg-green-800 text-neutral-content">
+            <button className="btn btn-ghost text-xl">LiveStock App</button>
+        </div>
+    </>
+  )
+}
+
+export default Nav
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { useState, useEffect, useContext } from "react"
+import { AuthContext } from "../context/auth.context"
+import Nav from "../components/Nav"
 
 const HomePage = () => {
+  const { isLoggedIn } = useContext(AuthContext)
   return (
+    isLoggedIn ? (
     <div>
       <h1>hello livestock app</h1>
     </div>
+  ) : (
+  <Nav />
+  )
   )
 }
 
