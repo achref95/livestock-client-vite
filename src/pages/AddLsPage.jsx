80ef5b503836f34b71322e0aaeebc606d9f68e38
Achref LAABIDI <abidi.achref95@gmail.com>
2023-12-03 21:33:04 +0100

    added loading to add ls page

--- src/pages/AddLsPage.jsx
+++ src/pages/AddLsPage.jsx
@@ -36,7 +36,17 @@ const AddLsPage = () => {
           console.log(error)
         }
     }
-
+  
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <span className="loading loading-ring loading-xs"></span>
+        <span className="loading loading-ring loading-sm"></span>
+        <span className="loading loading-ring loading-md"></span>
+        <span className="loading loading-ring loading-lg"></span>
+      </div>
+    )
+  }  
 
   return (
     <>
