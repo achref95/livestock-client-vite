8c933cdfb8fdd92af292fd39e55263fd129d4461
Achref LAABIDI <abidi.achref95@gmail.com>
2023-12-14 16:50:59 +0100

    nav bar changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,20 +7,57 @@ const Nav = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="navbar bg-green-800 text-neutral-content">
-      <button 
-        className="btn btn-ghost text-xl mr-auto"
-        onClick={() => {navigate("/")}}>
-        LiveStock App
-        </button>
-      {isLoggedIn &&       
-        <button className="btn btn-error" onClick={() => { 
-          logOutUser(); 
-          navigate("/"); 
-          }}>
-            Log Out
-        </button> }
+    // <div className="navbar bg-green-800 text-neutral-content">
+    //   <button 
+    //     className="btn btn-ghost text-xl mr-auto"
+    //     onClick={() => {navigate("/")}}>
+    //     LiveStock App
+    //     </button>
+    //   {isLoggedIn &&       
+    //     <button className="btn btn-error" onClick={() => { 
+    //       logOutUser(); 
+    //       navigate("/"); 
+    //       }}>
+    //         Log Out
+    //     </button> }
 
+    // </div>
+    <div className="drawer">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="w-full navbar bg-base-300">
+          <div className="flex-none lg:hidden">
+            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            </label>
+          </div> 
+          <div className="flex-1 px-2 mx-2" onClick={() => {navigate("/")}}>Live Stock App</div>
+          <div className="flex-none hidden lg:block">
+            <ul className="menu menu-horizontal">
+              {/* Navbar menu content here */}
+              <li><a>Navbar Item 1</a></li>
+              <li><a>Navbar Item 2</a></li>
+            </ul>
+          </div>
+        </div>
+      </div> 
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
+        <ul className="menu p-4 w-80 min-h-full bg-base-200">
+          {/* Sidebar content here */}
+          <li><button className="btn btn-active mb-4" onClick={() => {navigate("/add")}}>Add cattle</button></li>
+          <li><button className="btn btn-active mb-8" onClick={() => {navigate("/get")}}>Cattle stock</button></li>
+          <li> {isLoggedIn &&       
+            <button className="btn btn-error" onClick={() => { 
+              logOutUser(); 
+              navigate("/"); 
+              }}>
+                Log Out
+            </button> }
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,58 @@
-import { useState, useEffect, useContext } from "react"
-import { AuthContext } from "../context/auth.context"
-import Nav from "../components/Nav"
+import { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+import Nav from "../components/Nav";
 
 const HomePage = () => {
-  const { isLoggedIn } = useContext(AuthContext)
+  const { isLoggedIn } = useContext(AuthContext);
   return (
     isLoggedIn ? (
-    <div>
-      <h1>hello livestock app</h1>
-    </div>
-  ) : (
-  <Nav />
-  )
-  )
-}
+      <>
+        <Nav />
+        <div>
+          <h1>hello livestock app</h1>
+        </div>
+      </>
+    ) : (
+      <>
+    <div className="hero min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <div className="flex flex-row justify-center gap-x-4 items-center">
+            <h1 className="text-6xl font-bold">Fi𝓣rack</h1>
+          </div>
 
-export default HomePage
+          <p className="py-6 text-lg">
+            The best way to track your fitness progress! For the coach and
+            for clients!
+          </p>
+          <img src="/src/assets/cattle.jpg" alt="cow" />
+
+          {isLoggedIn ? (
+            <Link to="/overview">
+              <button className="btn btn-wide btn-primarybtn-outline mt-4">
+                Overview page
+              </button>
+            </Link>
+          ) : (
+            <div className="flex flex-col mt-4 space-y-4">
+              <Link to="/signup">
+                <button className="btn btn-wide btn-outline">
+                  Sign up
+                </button>
+              </Link>
+              <Link to="/login">
+                <button className="btn btn-info btn-wide">Login</button>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+      </>
+    )
+  );
+};
+
+export default HomePage;
+
